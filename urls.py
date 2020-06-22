ac0da745aada7e35bbffeb6c70c09005f2102660
Vibhav Srivastava <35532045+vibhav13@users.noreply.github.com>
2020-06-22 15:44:29 +0200

    Edit urls.py

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from search import views
+
+app_name = "search"
+urlpatterns = [
+    url(r"^$", views.SearchListView.as_view(), name="results"),
+    url(r"^suggestions/$", views.get_suggestions, name="suggestions"),
